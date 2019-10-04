bb722f8348ab508092429af68de4d709bf3e455b
Naman1011 <45920615+Naman1011@users.noreply.github.com>
2019-10-04 18:58:00 +0530

    Update mainmenu.cs

--- Assets/Assets/mainmenu.cs
+++ Assets/Assets/mainmenu.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+//Model_training
 public class mainmenu : MonoBehaviour {
 
 	// Use this for initialization
